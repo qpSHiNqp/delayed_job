82b2bf2ba65fc206c72bd8aab223fe1441eae3f0
Shintaro Tanaka <tallow@dirt.ninja>
2023-04-20 17:56:45 +0900

    Puts warn when Object#delay is called with block

--- lib/delayed/performable_method.rb
+++ lib/delayed/performable_method.rb
@@ -2,8 +2,9 @@ module Delayed
   class PerformableMethod
     attr_accessor :object, :method_name, :args
 
-    def initialize(object, method_name, args)
+    def initialize(object, method_name, args, &block)
       raise NoMethodError, "undefined method `#{method_name}' for #{object.inspect}" unless object.respond_to?(method_name, true)
+      warn "Ignored the block #{block.inspect} because it cannot be serialized." if block_given?
 
       if object.respond_to?(:persisted?) && !object.persisted?
         raise(ArgumentError, "job cannot be created for non-persisted record: #{object.inspect}")
